d806afc9c87a28f30aaf8e8dc3caea8cccc9e1b8
Senior YAML Developer <yamldeveloper@proton.me>
2022-08-18 08:48:43 +0300

    Update find_site_links.py

--- visualobjects.com/find_site_links.py
+++ visualobjects.com/find_site_links.py
@@ -12,12 +12,17 @@ session.headers.update({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
 })
 page = 1
+# https://clutch.co/your-project/requirements?source=visualobjects.com&utm_source=visualobjects&utm_medium=directory
+page_size = 16 + 1
 while 1:
     r = session.get(f'{BASE_CATEGORY}/{category}', dict(page=page))
     s = BeautifulSoup(r.text)
     buttons = s.find_all('div', class_='details-website-button')
     for btn in buttons:
-        print(btn.find('a')['href'], flush=True)
+        url = btn.find('a')['href']
+        if url.startswith('https://clutch.co/'):
+            continue
+        print(url, flush=True)
     if len(buttons) < page_size:
         break
     page += 1
