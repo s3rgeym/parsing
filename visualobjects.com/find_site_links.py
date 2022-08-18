1ac0264d99018a1744845dfb1904df139cad9275
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2022-08-18 08:49:19 +0300

    Update find_site_links.py

--- visualobjects.com/find_site_links.py
+++ visualobjects.com/find_site_links.py
@@ -6,7 +6,7 @@ BASE_URL = 'https://visualobjects.com/'
 if len(sys.argv) == 1:
     print('Usage:', sys.argv[0], '<category eg web-development>')
     sys.exit()
-category = sys.argv[1]
+CATEGORY = sys.argv[1]
 session = requests.session()
 session.headers.update({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
@@ -15,7 +15,7 @@ page = 1
 # https://clutch.co/your-project/requirements?source=visualobjects.com&utm_source=visualobjects&utm_medium=directory
 page_size = 16 + 1
 while 1:
-    r = session.get(f'{BASE_CATEGORY}/{category}', dict(page=page))
+    r = session.get(f'{BASE_CATEGORY}/{CATEGORY}', dict(page=page))
     s = BeautifulSoup(r.text)
     buttons = s.find_all('div', class_='details-website-button')
     for btn in buttons:
